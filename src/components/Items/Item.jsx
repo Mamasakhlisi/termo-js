85b661113feb922cd9820f2a7471cda790054a82
mamasakhlisi <datomamasakhlisi@gmail.com>
2020-04-25 14:55:16 +0400

    update

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react"
 import { data } from "../../fixed-data/data"
+import { dropData } from "../../fixed-data/dropdown"
 import Ace from "../Ace/Ace"
 import style from "./style.module.scss"
-import { CheckOutlined } from "@ant-design/icons"
+import { CheckOutlined, DownOutlined } from "@ant-design/icons"
 import giphy from "./giphy.gif"
+
 const Item = () => {
   const [state, setState] = useState({
     correct: 0,
     store: [],
   })
   const [active, setActive] = useState(1)
+  const [dropActive, setDropActive] = useState(0);
   const [aceValue, setAceValue] = useState("")
   const onChange = (newValue) => {
     setAceValue(newValue)
@@ -36,6 +39,8 @@ const Item = () => {
           codeResult: item.codeResult,
           result: item.result,
           storageResult: localStorage.getItem(`${item.id}-${item.topic}`),
+          description:item.description,
+          title:item.title
         })),
       })
     }, 3000)
@@ -43,18 +48,31 @@ const Item = () => {
   return (
     <div className={style.container}>
       <nav className={style.navigation}>
-        <ul className={style.ul}>
-          {state.store.map((item) => (
-            <li key={item.id} className={style.nav_li}>
-              <button onClick={() => setActive(item.id)}>
-                <span>თემა: {item.topic}</span>
-                {item.storageResult !== null && (
-                  <CheckOutlined style={{ color: "rgb(6, 170, 47)" }} />
-                )}
-              </button>
-            </li>
-          ))}
-        </ul>
+        {dropData.map((drop) => {
+          return (
+            <ul className={style.ul}>
+              <li className={style.nav_li}>
+                <button onClick={() => setDropActive(drop.id)}>
+                  <span>თემა: {drop.topic}</span>
+                  <DownOutlined />
+                </button>
+              </li>
+              {drop.id === dropActive && state.store.map(
+                (item) =>
+                  drop.topic === item.topic && (
+                    <li key={item.id} className={`${style.nav_li} ${style.dropdown_li}`}>
+                      <button onClick={() => setActive(item.id)}>
+                        <span className={`${item.title.length > 30 && style.title}`}>{item.title}</span>
+                        {item.storageResult !== null && (
+                          <span className={style.check}><CheckOutlined style={{ color: "rgb(6, 170, 47)" }} /></span>
+                        )}
+                      </button>
+                    </li>
+                  )
+              )}
+            </ul>
+          )
+        })}
       </nav>
       {state.store.map((item) => {
         const topic = `${item.id}-${item.topic}`
@@ -62,10 +80,11 @@ const Item = () => {
           item.id === active && (
             <div className={style.constructor} key={item.id}>
               <div className={style.example}>
-                <h4>მითითება: {item.label}</h4>
-                <h5>მაგალითი: {item.example}</h5>
-                {item.example2 && <h5>მაგალითი: {item.example2}</h5>}
-                {item.example3 && <h5>მაგალითი: {item.example3}</h5>}
+                <h3>მითითება: {item.label}</h3>
+                <p>{item.description}</p>
+                <h4>მაგალითი: {item.example}</h4>
+                {item.example2 && <h4>მაგალითი: {item.example2}</h4>}
+                {item.example3 && <h4>მაგალითი: {item.example3}</h4>}
               </div>
               <Ace
                 onChange={onChange}
---
+++ src/fixed-data/dropdown.js
@@ -0,0 +1,10 @@
+export const dropData = [
+    {
+        id:1,
+        topic: 'ცვლადი'
+    },
+    {
+        id:2,
+        topic: 'ფუნქცია'
+    },
+]
