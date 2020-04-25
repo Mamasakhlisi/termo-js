47a13a83ecab55e42e6769a807be62d9b6c02dbf
mamasakhlisi <datomamasakhlisi@gmail.com>
2020-04-25 17:12:06 +0400

    update

--- src/components/Ace/AceStyle.js
+++ src/components/Ace/AceStyle.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
 export const AceStyle = styled.div`
-width:1000px;
-max-width:100%;
-margin-top:-10px;
+    width:1000px;
+    max-width:100%;
+    margin-top:-10px;
+    @media (max-width:1500px) {
+        width:850px;
+    }
+    @media (max-width: 1310px) {
+        width: 740px;
+    }
     #editor {
         position:absolute;
     }
